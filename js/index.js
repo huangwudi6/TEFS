332ef83d15a31748d85e70741d5448e20e53f011
huangwudi <huangwudi6@gmail.com>
2021-04-21 13:01:29 +0800

    .

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function signin(){
     // alert("lajskd")
     var usermail=document.getElementById("floatingInput")
     var password=document.getElementById("floatingPassword")
-    
+    console.log("login")
     $.ajax({
         method:"GET",
         url:"127.0.0.1/userctrl?action=login&username="+ usermail +"&password="+ password,
